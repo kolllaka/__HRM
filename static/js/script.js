8329cf94defd4190492553ed1a4bec00c464f961
Denis <kotshredengera@gmail.com>
2023-04-25 01:47:09 +0300

    class Jobber

--- static/js/script.js
+++ static/js/script.js
@@ -9,12 +9,45 @@ class Jobber {
 	#render() {
 		this.$el.classList.add("list__body");
 
-		this.$el.innerHTML = getJobberTemplate(this.options)
+		this.$el.innerHTML = getJobberTemplate(this.options.jobber)
 	}
 }
 
 const getJobberTemplate = (options = []) => {
-	options.forEach((option) => {
-
+	let jobbersTemplate = options.map((option, index) => {
+		return `
+		<ul class="list__row">
+			<li class="list__item">${index + 1}</li>
+			<li class="list__item">${option.Name}</li>
+			<li class="list__item">${option.SecondName}</li>
+			<li class="list__item">${option.Surname}</li>
+			<li class="list__item">${option.Job}</li>
+		</ul>
+		`
 	})
+
+	return jobbersTemplate.join('')
 }
+
+const jobber = new Jobber("jobber", {
+	jobber: [
+		{
+			Name: "Петров",
+			SecondName: "Пётр",
+			Surname: "Петрович",
+			Job: "грузчик"
+		},
+		{
+			Name: "Вадим",
+			SecondName: "Радионович",
+			Surname: "Ежак",
+			Job: "грузчик"
+		},
+		{
+			Name: "Гига",
+			SecondName: "Чад",
+			Surname: "Ёбырев",
+			Job: "модер"
+		}
+	]
+})
